5636829c6efb432b4eb6db9f11c70df584233846
mgarcp13 <mgarcp13@gmail.com>
2022-07-23 14:39:06 +0200

    Include web application

---
+++ web-application/aaset/controllers/main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+def load():
+    response.view = 'main/load.html'
+    return dict()
+
+def report():
+    import json
+    import subprocess
+    import os
+    from random import seed
+    from random import randint
+    
+    response.view = 'main/report.json'
+    
+    seed()
+    exec_dir="exec."+str(randint(0,1000))
+    
+    file = request.vars.source.file
+    filename = request.vars.source.filename
+    output_file=open("/tmp/"+filename, "wb").write(file.read())
+    
+    os.chdir('/home/mario/automatic_ada_symbolic_execution/')
+    
+    subprocess.run(["./automatic_klee_execution.sh",request.vars.module,"/tmp/"+filename,exec_dir])
+    
+    report=open(exec_dir+"/output/report/report.json","r").read()
+    subprocess.run(["rm","-rf",exec_dir])
+    
+    return dict(reporte=json.JSONDecoder().decode(report))
